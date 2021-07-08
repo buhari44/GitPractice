daf767b21b23b2ef3e88123b6e6590c032147dc1
buhari44 <adeyemibuari@gmail.com>
2021-07-08 14:53:40 +0100

    add Password features

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("git&github")
+const passwordCheck = document.getElementById("password");
+const confirmPassword = document.getElementById("Cpassword");
+console.log(passwordCheck);
+const submitForm = () => {
+  passwordCheck === confirmPassword
+    ? alert("Your password is correct")
+    : alert("incorrect password");
+};
